shidenggui/logseq-dev

[wip] feat: file sync

src/main/frontend/util/persist_var.cljs
@@ -16,26 +16,25 @@
   (-save [this]))
 
 (deftype PersistVar [*value location]
+  Object
+  (reset-value! [_ new graph]
+    (reset! *value (assoc-in @*value [graph :value] new)))
+
   ILoad
   (-load [_]
-    (state/add-watch-state (keyword (str "persist-var/" location))
-                           (fn [k r o n]
-                             (let [repo (state/get-current-repo)]
-                               (when (and
-                                      (not (get-in @*value [repo :loaded?]))
-                                      (get-in n [:nfs/user-granted? repo]))
-                                 (p/let [content (fs/read-file
-                                                  (config/get-repo-dir (state/get-current-repo))
-                                                  (load-path location))]
-                                   (when-let [content (and (some? content)
-                                                           (try (cljs.reader/read-string content)
-                                                                (catch js/Error e
-                                                                  (println (util/format "load persist-var failed: %s"  (load-path location)))
-                                                                  (js/console.dir e))))]
-                                     (swap! *value (fn [o]
-                                                     (-> o
-                                                         (assoc-in [repo :loaded?] true)
-                                                         (assoc-in [repo :value] content)))))))))))
+    (let [repo (state/get-current-repo)]
+      (p/let [content (fs/read-file
+                       (config/get-repo-dir (state/get-current-repo))
+                       (load-path location))]
+        (when-let [content (and (some? content)
+                                (try (cljs.reader/read-string content)
+                                     (catch js/Error e
+                                       (println (util/format "load persist-var failed: %s"  (load-path location)))
+                                       (js/console.dir e))))]
+          (swap! *value (fn [o]
+                          (-> o
+                              (assoc-in [repo :loaded?] true)
+                              (assoc-in [repo :value] content))))))))
 
   ISave
   (-save [_]
@@ -51,7 +50,20 @@
 
   IReset
   (-reset! [o new-value]
-    (swap! *value (fn [o] (assoc-in @*value [(state/get-current-repo) :value] new-value)))))
+    "Deprecated - use (.reset-value! o) instead."
+    (swap! *value (fn [o] (assoc-in @*value [(state/get-current-repo) :value] new-value))))
+
+
+  IPrintWithWriter
+  (-pr-writer [o w opts]
+    (write-all w (str "#PersistVar[" @*value ", loc: " location "]"))))
+
+
+(def *all-persist-vars (atom []))
+
+(defn load-vars []
+  (doseq [var @*all-persist-vars]
+    (-load var)))
 
 (defn persist-var [init-value location]
   "This var is stored at logseq/LOCATION.edn"
@@ -59,7 +71,7 @@
                                  {:value init-value
                                   :loaded? false}})
                           location)]
-    (-load var)
+    (swap! *all-persist-vars conj var)
     var))
 
 (defn persist-save [v]

Vtec234/logseq

src/main/frontend/util/persist_var.cljs
@@ -17,26 +17,25 @@
   (-save [this]))
 
 (deftype PersistVar [*value location]
+  Object
+  (reset-value! [_ new graph]
+    (reset! *value (assoc-in @*value [graph :value] new)))
+
   ILoad
   (-load [_]
-    (state/add-watch-state (keyword (str "persist-var/" location))
-                           (fn [_k _r _o n]
-                             (let [repo (state/get-current-repo)]
-                               (when (and
-                                      (not (get-in @*value [repo :loaded?]))
-                                      (get-in n [:nfs/user-granted? repo]))
-                                 (p/let [content (fs/read-file
-                                                  (config/get-repo-dir (state/get-current-repo))
-                                                  (load-path location))]
-                                   (when-let [content (and (some? content)
-                                                           (try (reader/read-string content)
-                                                                (catch js/Error e
-                                                                  (println (util/format "load persist-var failed: %s"  (load-path location)))
-                                                                  (js/console.dir e))))]
-                                     (swap! *value (fn [o]
-                                                     (-> o
-                                                         (assoc-in [repo :loaded?] true)
-                                                         (assoc-in [repo :value] content)))))))))))
+    (let [repo (state/get-current-repo)]
+      (p/let [content (fs/read-file
+                       (config/get-repo-dir (state/get-current-repo))
+                       (load-path location))]
+        (when-let [content (and (some? content)
+                                (try (cljs.reader/read-string content)
+                                     (catch js/Error e
+                                       (println (util/format "load persist-var failed: %s"  (load-path location)))
+                                       (js/console.dir e))))]
+          (swap! *value (fn [o]
+                          (-> o
+                              (assoc-in [repo :loaded?] true)
+                              (assoc-in [repo :value] content))))))))
 
   ISave
   (-save [_]
@@ -52,7 +51,20 @@
 
   IReset
   (-reset! [_o new-value]
-    (swap! *value (fn [_o] (assoc-in @*value [(state/get-current-repo) :value] new-value)))))
+    "Deprecated - use (.reset-value! o) instead."
+    (swap! *value (fn [_o] (assoc-in @*value [(state/get-current-repo) :value] new-value))))
+
+
+  IPrintWithWriter
+  (-pr-writer [o w opts]
+    (write-all w (str "#PersistVar[" @*value ", loc: " location "]"))))
+
+
+(def *all-persist-vars (atom []))
+
+(defn load-vars []
+  (doseq [var @*all-persist-vars]
+    (-load var)))
 
 (defn persist-var
   "This var is stored at logseq/LOCATION.edn"
@@ -61,7 +73,7 @@
                                  {:value init-value
                                   :loaded? false}})
                           location)]
-    (-load var)
+    (swap! *all-persist-vars conj var)
     var))
 
 (defn persist-save [v]